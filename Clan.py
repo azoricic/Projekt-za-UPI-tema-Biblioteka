9ed4b1246d244c9e3ac837f75976aee52a93b22d
azoricic <58751370+azoricic@users.noreply.github.com>
2019-12-16 15:00:13 +0100

    Klase za podizanje i vraćanje knjiga pojedinog člana

---
+++ Clan.py
@@ -0,0 +1,19 @@
+class Clan: 
+
+      def zatrazenaKnjiga(self): 
+
+            print("Napišite ime knjige koju želite podignuti: ") 
+
+            self.knjiga=input() 
+
+            return self.knjiga 
+
+  
+
+      def vratiKnjigu(self): 
+
+            print("Napišite ime knjige koju želite vratiti: ") 
+
+            self.knjiga=input() 
+
+            return self.knjiga 
