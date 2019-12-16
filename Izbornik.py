f028cc5d19ca6812fdf2db4d1ad20c9442f923b3
anajosipovic <58830608+anajosipovic@users.noreply.github.com>
2019-12-16 15:02:24 +0100

    Izbornik za biranje opcija

---
+++ Izbornik.py
@@ -0,0 +1,39 @@
+import sys
+def main():             
+
+      biblioteka=Biblioteka(["Programiranje u Pythonu","Harry Potter i kamen mudraca","Družba Pere Kvržice","Pale sam na svijetu"]) 
+
+      clan=Clan() 
+
+      ok=False 
+
+      while ok==False: 
+
+            print(""" ======BIBLIOTEKA======= 
+
+                  1. Prikaži dostupne knjige 
+
+                  2. Podigni knjigu
+
+                  3. Vrati knjigu 
+
+                  4. Zatvori program 
+
+                  """) 
+
+            izbor=int(input("Izaberi opciju:")) 
+
+            if izbor==1: 
+
+                        biblioteka.dostupneKnjigePrikaz() 
+
+            elif izbor==2: 
+
+                        biblioteka.posudiKnjigu(clan.zatrazenaKnjiga()) 
+
+            elif izbor==3: 
+
+                        biblioteka.dodajKnjigu(clan.vracenaKnjiga()) 
+
+            elif izbor==4:
+                        sys.exit() 
